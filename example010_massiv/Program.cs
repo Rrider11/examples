e52de17d98f3daf4c6e51c8b47f7e27329c0c425
Manakov Aleksandr <manakov.shura@yandex.ru>
2022-08-09 01:33:57 +0300

    чё то поделали

---
+++ example010_massiv/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {5, 12, 13, 41, 56, 16, 75, 41, 18, 96};
+
+int n = array.Length;
+int find = 41;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    } 
+    //index = index + 1
+    index++;
+}
